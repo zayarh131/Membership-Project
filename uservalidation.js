b5e36a8e9bcb83dddf7fad9897b1b23401ff0438
Zay-invalid <zayarhtet797@gmail.com>
2020-11-18 17:50:48 +0630

    fix bugs 6.3

--- uservalidation.js
+++ uservalidation.js
@@ -3,6 +3,8 @@ function validateUsername() {
     var password = document.getElementById("password").value;
     var usernameError = document.getElementById("invalid_username");
     var passwordError = document.getElementById("invalid_password");
+    username.trim();
+    password.trim();
     usernameError.innerHTML="";
     passwordError.innerHTML="";
     var expr = /^[a-zA-Z0-9_]{6,20}$/;
@@ -23,18 +25,29 @@ function addRow() {
     var team = document.getElementById("teamID").value;
     var edit = '<button onclick="editRow()"><i class="fas fa-pen"></i></button> <button onclick="deleteRow(this)"><i class="fas fa-trash"></i></button>';
 
-    var valueArray = [username,name,team,edit];
+    username.trim();
+    name.trim();
 
-    var tr = document.createElement("tr");
+    var invalid = document.getElementById("invalid");
+    
+    if(username == "" || name== "" || (username == "" && name == "")) {
+        invalid.style.display="block";
+        invalid.innerHTML="Please Enter Username and Name";
+    } else {
+        invalid.style.display="none";
+        var valueArray = [username,name,team,edit];
 
-    var index;
-    for (index=0; index < valueArray.length; index++) {
-        var td = document.createElement("td");
-        td.innerHTML=valueArray[index];
-        tr.appendChild(td);
+        var tr = document.createElement("tr");
+    
+        var index;
+        for (index=0; index < valueArray.length; index++) {
+            var td = document.createElement("td");
+            td.innerHTML=valueArray[index];
+            tr.appendChild(td);
+        }
+    
+        document.querySelector("tbody").appendChild(tr);
     }
-
-    document.querySelector("tbody").appendChild(tr);
 
     document.getElementById("usernameID").value="";
     document.getElementById("nameID").value="";
