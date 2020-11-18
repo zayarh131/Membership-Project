7d28b497ca64a1d818eea7c51791a5a51220517e
Zay-invalid <zayarhtet797@gmail.com>
2020-11-18 13:50:37 +0630

    add js

--- uservalidation.js
+++ uservalidation.js
@@ -16,8 +16,34 @@ function validateUsername() {
     }
 }
 
-function editRow() {
 
+function addRow() {
+    var username = document.getElementById("usernameID").value;
+    var name = document.getElementById("nameID").value;
+    var team = document.getElementById("teamID").value;
+    var edit = '<button onclick="editRow()"><i class="fas fa-pen"></i></button>';
+    var remove = '<button onclick="deleteRow()"><i class="fas fa-trash"></i></button>';
+
+    var valueArray = [username,name,team,edit,remove];
+
+    var tr = document.createElement("tr");
+
+    var index;
+    for (index=0; index < valueArray.length; index++) {
+        var td = document.createElement("td");
+        td.innerHTML=valueArray[index];
+        tr.appendChild(td);
+    }
+
+    document.querySelector("tbody").appendChild(tr);
+
+    document.getElementById("usernameID").value="";
+    document.getElementById("nameID").value="";
+    document.getElementById("teamID").value="none";
+}
+
+function editRow() {
+    
 }
 
 function deleteRow() {
